de831c164c1bb0b6403d6d4a197710a779fce9f0
zhuaishao <1716816143@qq.com>
2024-01-31 21:30:12 +0800

    add new daily question

---
+++ daily question/2670/solution.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> distinctDifferenceArray(vector<int>& nums) {
+        unordered_set<int> us;
+        int n = nums.size();
+        vector<int> pred(n+1);
+        for(int i = n-1;i >= 0;i--){
+            if(us.find(nums[i]) == us.end()){
+                us.insert(nums[i]);
+            }
+            pred[i] = us.size();            
+        }
+        us.clear();
+        for(int i = 0;i < n;i++){
+            if(us.find(nums[i]) == us.end()){
+                us.insert(nums[i]);
+            }
+            pred[i] = us.size() - pred[i+1];
+        }
+        pred.pop_back();
+        return pred;
+    }
+};
