26b95c4d09ac88d492fb49750b94dc282e79ccf8
zhuaishao <1716816143@qq.com>
2024-02-28 21:35:00 +0800

    add new daily question

---
+++ daily question/2673/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int minIncrements(int n, vector<int>& cost) {
+        int ans = 0;
+        for (int i = n - 2; i > 0; i -= 2) {
+            ans += abs(cost[i] - cost[i + 1]);
+            cost[i / 2] += max(cost[i], cost[i + 1]);
+        }
+        return ans;
+    }
+};
+
